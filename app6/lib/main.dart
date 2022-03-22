9f0f8beba491d0be177327ae8c4209663d71c43c
arturobl00 <arturobl00@msn.com>
2022-03-22 12:27:22 -0600

    22/03

--- app6/lib/main.dart
+++ app6/lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -27,6 +31,23 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController name = TextEditingController();
+  TextEditingController email = TextEditingController();
+
+  create() async {
+    try {
+      await FirebaseFirestore.instance
+          .collection("datos")
+          .doc(email.text)
+          .set({"name": name.text, "email": email.text});
+      // ignore: avoid_print
+      print("Dato Registrado");
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +59,7 @@ class _HomeState extends State<Home> {
           child: Column(
             children: [
               TextField(
+                controller: name,
                 decoration: InputDecoration(
                     labelText: "User Name",
                     border: OutlineInputBorder(
@@ -47,6 +69,7 @@ class _HomeState extends State<Home> {
                 height: 22.0,
               ),
               TextField(
+                controller: email,
                 decoration: InputDecoration(
                     labelText: "Email User",
                     border: OutlineInputBorder(
@@ -59,7 +82,11 @@ class _HomeState extends State<Home> {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        create();
+                        name.clear();
+                        email.clear();
+                      },
                       style:
                           TextButton.styleFrom(backgroundColor: Colors.green),
                       child: const Text("Create")),
